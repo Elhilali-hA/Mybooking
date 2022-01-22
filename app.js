6ed552141cf878588988571e2035d6a52ce467b1
Elhilali-hA <hamzaelhilali8@gmail.com>
2022-01-22 18:20:57 +0100

    post user

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ const PORT = 8000
 app.listen(PORT, console.log('hello my booking' + ' ' + `you are listen at ${PORT}`))
 
 dotenv.config()
-mongoose.connect(process.env.DB_CONNECT, {
-    useNewUrlParser: true
-}).then(() => {console.log('connect to db')})
+mongoose.connect(process.env.DB, ).then(() => {console.log('connect to db')})
 .catch((err) => {console.log(err)})
 
 app.use(express.json())
