4dea35b76721af5aff87ff09342f563abd385097
Elhilali-hA <hamzaelhilali8@gmail.com>
2022-01-22 02:32:02 +0100

    app.js / model

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+
+
+
+
+const PORT = 8000
+app.listen(PORT, console.log('hello my booking' + ' ' + `you are listen at ${PORT}`))
+
+dotenv.config()
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true
+}).then(() => {console.log('connect to db')})
+.catch((err) => {console.log(err)})
+
+app.use(express.json())
+const authroute = require('./routes/auth')
+app.use('/', authroute)
+
+
